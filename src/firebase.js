675e07a77460d87510ce08681c1adcec0f42683a
saravanan <saravananr7337@gmail.com>
2023-02-02 14:37:18 +0530

    update F_B

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,17 @@ import 'firebase/compat/firestore';
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//end to3 to 4
 const firebaseConfig = {
-    apiKey: "AIzaSyCNfRO4UbO10tG-bXAfOncpcbGmSyGx1q8",
-    authDomain: "messenger-af020.firebaseapp.com",
-    projectId: "messenger-af020",
-    storageBucket: "messenger-af020.appspot.com",
-    messagingSenderId: "194138140127",
-    appId: "1:194138140127:web:f84f06e7493d0cace4e86a",
-    measurementId: "G-Z6EBPSXW1H"
-  };
+  apiKey: "AIzaSyBnwg6PP2Hd13Yxmq2Y5eeuy_ocllC-1fE",
+  authDomain: "new-messenger-24ae7.firebaseapp.com",
+  projectId: "new-messenger-24ae7",
+  storageBucket: "new-messenger-24ae7.appspot.com",
+  messagingSenderId: "889096521751",
+  appId: "1:889096521751:web:69115921c776155a8df452",
+  measurementId: "G-9LS72069RT"
+};
   const firebaseApp=firebase.initializeApp(firebaseConfig)
  
   
